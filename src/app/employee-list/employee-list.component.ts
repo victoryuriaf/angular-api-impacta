d42a4d75960a6e7fe8a9b07d47141c35edbfd319
Victor Yuri <victoryuri.af@gmail.com>
2022-01-05 17:16:13 -0300

    employee-list implementation

--- src/app/employee-list/employee-list.component.ts
+++ src/app/employee-list/employee-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RestApiService } from '../shared/rest-api.service';
 
 @Component({
   selector: 'app-employee-list',
@@ -6,10 +7,32 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employee-list.component.css']
 })
 export class EmployeeListComponent implements OnInit {
+// primeira parte - criar uma propriedade para ser a colecao iteravel de dados
+  listEmployees: any = []
 
-  constructor() { }
+// segunda parte - a referencia de instancia da REST API
+  constructor(
+    public restApi: RestApiService
+  ) { }
 
+// terceira parte - priorizando a chamada da funcao loadEmployees para gerar uma lista no component
   ngOnInit(): void {
+    this.loadEmployees()
+  }
+
+// quarta parte - criar a funcao para acessar a REST API e carregar todos os dados no component
+  loadEmployees(){
+    return this.restApi.getEmployees().subscribe((data:{}) => {
+      this.listEmployees = data
+    })
+  }
+
+  // quinta parte - funcao para acessar a REST API -> excluir um registro
+  deleteEmployee(id:any){
+    if(window.confirm('Do you really want to delete the record?')){
+      this.restApi.deleteEmployee(id).subscribe(data => {
+        this.loadEmployees()})
+    }
   }
 
 }
